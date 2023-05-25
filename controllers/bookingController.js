ce8b0e3977964cb6cdafe012f15f9f81dc8f8045
Jack Do <jackydo1974@gmail.com>
2023-05-25 00:32:25 -0700

    fix booking with webhook

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -9,57 +9,36 @@ exports.getCheckoutSession = catchAsync(async(req, res, next) =>{
   // 1) Get the currently booked tour
  const tour = await Tour.findById(req.params.tourId)
 
-//  const transformedItems = [{
-//      quantity: 1,
-//      price_data: {
-//          currency: "usd",
-//          unit_amount: tour.price * 100,
-//          product_data: {
-//              name: `${tour.name} Tour`,
-//              description: tour.description, //description here
-//              images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`], //only accepts live images (images hosted on the internet),
-//          },
-//      },
-//  }];
+ const transformedItems = [{
+     quantity: 1,
+     price_data: {
+         currency: "usd",
+         unit_amount: tour.price * 100,
+         product_data: {
+             name: `${tour.name} Tour`,
+             description: tour.description, //description here
+             images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`], //only accepts live images (images hosted on the internet),
+         },
+     },
+ }];
 
   // 2) Create checkout session
-//  const session = await stripe.checkout.sessions.create({
-//      payment_method_types: ['card'],
-//      // success_url: `${req.protocol}://${req.get('host')}/`, //user will be redirected to this url when payment is successful. home page
-//      // cancel_url: `${req.protocol}://${req.get('host')}/${tour.slug}`, //user will be redirected to this url when payment has an issue. tour page (previous page)
-//     //  success_url: `${req.protocol}://${req.get('host')}/?tour=${tour.id}&user=${req.user.id}&price=${tour.price}`,
-//     //  cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
-//     success_url: `${req.protocol}://${req.get('host')}/`,
-//     cancel_url: `${req.protocol}://${req.get('host')}/${tour.slug}`,
-//      customer_email: req.user.email,
-//      client_reference_id: req.params.tourId, //this field allows us to pass in some data about this session that we are currently creating.
-//      line_items: transformedItems,
-//      mode: 'payment',
-
-//  });
-
  const session = await stripe.checkout.sessions.create({
-  payment_method_types: ['card'],
-  // success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
-  //   req.params.tourId
-  // }&user=${req.user.id}&price=${tour.price}`,
-  success_url: `${req.protocol}://${req.get('host')}`,
-  cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
-  customer_email: req.user.email,
-  client_reference_id: req.params.tourId,
-  display_items: [
-    {
-      name: `${tour.name} Tour`,
-      description: tour.summary,
-      images: [
-        `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`
-      ],
-      amount: tour.price * 100,
-      currency: 'usd',
-      quantity: 1
-    }
-  ]
-});
+     payment_method_types: ['card'],
+     // success_url: `${req.protocol}://${req.get('host')}/`, //user will be redirected to this url when payment is successful. home page
+     // cancel_url: `${req.protocol}://${req.get('host')}/${tour.slug}`, //user will be redirected to this url when payment has an issue. tour page (previous page)
+    //  success_url: `${req.protocol}://${req.get('host')}/?tour=${tour.id}&user=${req.user.id}&price=${tour.price}`,
+    //  cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+    success_url: `${req.protocol}://${req.get('host')}/`,
+    cancel_url: `${req.protocol}://${req.get('host')}/${tour.slug}`,
+     customer_email: req.user.email,
+     client_reference_id: req.params.tourId, //this field allows us to pass in some data about this session that we are currently creating.
+     line_items: transformedItems,
+     mode: 'payment',
+
+ });
+
+
 
   // 3) Create session as response
   res.status(200).json({
@@ -71,7 +50,7 @@ exports.getCheckoutSession = catchAsync(async(req, res, next) =>{
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items[0].amount / 100;
+  const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 
 };
